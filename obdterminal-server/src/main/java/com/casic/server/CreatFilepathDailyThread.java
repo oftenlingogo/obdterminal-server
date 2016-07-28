0b9c16687c5d7aeb3d7c4fa0712f274d1b98c716
oftenlin <oftenlin@163.com>
2016-07-28 11:43:46 +0800

    又一个冲突

--- obdterminal-server/src/main/java/com/casic/server/CreatFilepathDailyThread.java
+++ obdterminal-server/src/main/java/com/casic/server/CreatFilepathDailyThread.java
@@ -11,6 +11,7 @@ import org.apache.log4j.Logger;
 import com.casic.utils.SystemConstants;
 
 public class CreatFilepathDailyThread extends TimerTask {
+	// 尼玛 又一个冲突
 	private final static Logger LOGGER = Logger.getLogger(OBDServerApp.class);
 	public void run() {
 		String filePath =SystemConstants.RT_DATAPATH;
