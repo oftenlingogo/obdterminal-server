56e5e8de851cf2d5e984cbeee63bb52a88c16acc
oftenlin <oftenlin@163.com>
2016-07-28 11:34:09 +0800

    测试提交

--- obdterminal-server/src/main/java/com/casic/server/CreatFilepathDailyThread.java
+++ obdterminal-server/src/main/java/com/casic/server/CreatFilepathDailyThread.java
@@ -11,7 +11,7 @@ import org.apache.log4j.Logger;
 import com.casic.utils.SystemConstants;
 
 public class CreatFilepathDailyThread extends TimerTask {
-	// 荆长
+	// 荆长林
 	private final static Logger LOGGER = Logger.getLogger(OBDServerApp.class);
 	public void run() {
 		String filePath =SystemConstants.RT_DATAPATH;
